406d675e4f6094d408e22c71a035c4022b5b0daa
Arihant Bansal <17180950+arihantbansal@users.noreply.github.com>
2021-09-18 19:15:40 +0530

    Index.js readded

--- src/App.js
+++ src/App.js
@@ -7,23 +7,26 @@ import {
   Grid,
   theme,
 } from '@chakra-ui/react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { ColorModeSwitcher } from './ColorModeSwitcher';
 import { Logo } from './Logo';
 
 const App = () => {
   return (
     <ChakraProvider theme={theme}>
-      <Box textAlign="center" fontSize="xl">
-        <Grid minH="100vh" p={3}>
-          <ColorModeSwitcher justifySelf="flex-end" />
-          <VStack spacing={8}>
-            <Logo h="40vmin" pointerEvents="none" />
-            <Text fontSize="xl" fontWeight="bold">
-              Alchemist
-            </Text>
-          </VStack>
-        </Grid>
-      </Box>
+      <Router>
+        <Box textAlign="center" fontSize="xl">
+          <Grid minH="100vh" p={3}>
+            <ColorModeSwitcher justifySelf="flex-end" />
+            <VStack spacing={8}>
+              <Logo h="40vmin" pointerEvents="none" />
+              <Text fontSize="xl" fontWeight="bold">
+                Alchemist
+              </Text>
+            </VStack>
+          </Grid>
+        </Box>
+      </Router>
     </ChakraProvider>
   );
 };
