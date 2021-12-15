f68b7469678412cb8962140b4a2cf4718531597c
MysteriousAcadia <anshumansingh64@gmail.com>
2021-12-15 15:33:03 +0530

    Add GraphQL

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { ChakraProvider, Box, VStack, Grid, theme } from "@chakra-ui/react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import NavBar from "components/NavBar";
@@ -7,8 +7,11 @@ import BalanceCards from "pages/BalanceCards";
 import Footer from "components/Footer";
 import CraftPage from "pages/CraftPage";
 import Map from "pages/Map";
-
+import getSensorData from "config/graphql";
 const App = () => {
+  useEffect(()=>{
+    getSensorData({});
+  },[])
   return (
     <ChakraProvider theme={theme}>
       <Router>
---
+++ src/QuerySensorData.js
@@ -0,0 +1,61 @@
+// querySensorData.js
+
+const ApolloClient = require('apollo-client').ApolloClient;
+const createHttpLink = require("apollo-link-http").createHttpLink;
+const InMemoryCache = require("apollo-cache-inmemory").InMemoryCache;
+const crossFetch = require('cross-fetch');
+const gql = require('graphql-tag');
+
+// Include protobuf to decode the device data into JSON
+const protobuf = require("protobufjs");
+
+// TruStream Subgraph Endpoint to fetch verified telemetry
+const TRUSTREAM_SUBGRAPH = "https://subgraph.iott.network/subgraphs/name/iotex/pebble-subgraph";
+
+// GraphQL Query to fetch the first 10 data points, 
+// from device with IMEI 100000000000005
+const DEVICE_TELEMETRY_QUERY =
+    gql`
+    {
+        deviceRecords(where: { imei: "100000000000005" }, first : 10) {
+            raw # Protobuf encoded sensors values
+            imei
+            signature
+        }
+    }
+    `
+RunApp();
+
+async function RunApp() {
+
+    // Create the GraphQL client
+    const client = new ApolloClient({
+        link: createHttpLink({
+            uri: TRUSTREAM_SUBGRAPH,
+            fetch: crossFetch
+        }),
+        cache: new InMemoryCache()
+    });
+
+    // Run the query
+    const queryResult = await client.query({ query: DEVICE_TELEMETRY_QUERY });
+
+    // Log all the data points to the console
+    console.log(queryResult.data.deviceRecords);
+
+    // Let's decode the first data message 
+    const dataPoint = queryResult.data.deviceRecords[0];
+
+    // Load the protobuf definition, it's on GitHub at
+    // https://github.com/iotexproject/iott-dapp-example/blob/main/service/proto/pebble.proto
+    const pebbleProtoDef = await protobuf.load("pebble.proto");
+    // We need the SensorData proto definition, let's import it
+    const SensorData = pebbleProtoDef.lookupType('SensorData');
+    // Decode the telemetry
+    const encodedTelemetry = dataPoint.raw.replace(/0x/g, '');
+    const telemetry = SensorData.decode(Buffer.from(encodedTelemetry,"hex"));
+    
+    // Log the telemetry
+    console.log(telemetry);
+
+}
